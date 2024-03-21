6ee0a30315643171aed567a90b88ac3c78261956
Chillout111 <164400719+Chillout111@users.noreply.github.com>
2024-03-22 02:26:23 +0700

    Update loader.lua

--- main/loader.lua
+++ main/loader.lua
@@ -1,7 +1,5 @@
-repeat task.wait() until game:IsLoaded()
-
 if game.PlaceId == 8737899170 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TrungB2/Skid/BestSkid/Event/script1d.lua"))()
-elseif game.PlaceId == 16498369169 then -- and whatever parameter u want ig
-    game.ReplicatedStorage.Network.World1Teleport:InvokeServer()
+elseif game.PlaceId == 16498369169 then 
+    game.ReplicatedStorage.Network.World2Teleport:InvokeServer()
 end
