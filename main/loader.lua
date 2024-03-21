d7d68a3856727474b2fcb60cc896ce4bad1d30f4
Chillout111 <164400719+Chillout111@users.noreply.github.com>
2024-03-22 03:56:42 +0700

    Update loader.lua

--- main/loader.lua
+++ main/loader.lua
@@ -1,5 +1,1 @@
-if game.PlaceId == 8737899170 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TrungB2/Skid/BestSkid/Event/script1d.lua"))()
-elseif game.PlaceId == 16498369169 then 
-    game.ReplicatedStorage.Network.World2Teleport:InvokeServer()
-end
+
